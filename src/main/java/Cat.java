54a3ed3196d2383fff0fbe1e8f49b609ecfb7fcc
Muipol <mikhail.shashev144@gmail.com>
2023-03-19 14:04:45 +0700

    Compete

---
+++ src/main/java/Cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+
+    private String value;
+
+    public Cat() {
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
